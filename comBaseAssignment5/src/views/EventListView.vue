159bac28e387f0a24cbb6ffa34873398288180e6
Jookmaste <punnlaowatchara@gmail.com>
2026-08-08 23:18:26 +0700

    2.2 tailwind style

--- comBaseAssignment5/src/views/EventListView.vue
+++ comBaseAssignment5/src/views/EventListView.vue
@@ -47,39 +47,40 @@
 <template>
 
   <h1>Event For Good</h1>
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <EventInfo v-for="event in events" :key="event.id" :event="event" />
 
-    <div class="pagination">
+    <div class="flex items-center">
       <RouterLink
       id="page-prev"
         :to="{ name: 'event-list-view', query: { page: page - 1} }"
         rel="prev"
         v-if="page != 1"
-        >&#60; Prev Page</RouterLink>
+        >&#60; | Prev Page | </RouterLink>
 
       <RouterLink
         id="page-next"
         :to="{name: 'event-list-view', query: { page: page + 1 } }"
         rel="next"
         v-if="hasNextPage"
-        >Next Page &#62;</RouterLink>
+        > | Next Page | &#62;</RouterLink>
     </div>
   </div>
 </template>
 
 <style scoped>
-.events {
+
+/* .events {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
+} */
 
-.pagination {
+/* .pagination {
   display: flex;
   width: 290px;
-}
+} */
 
 .pagination a {
   flex: 1;
