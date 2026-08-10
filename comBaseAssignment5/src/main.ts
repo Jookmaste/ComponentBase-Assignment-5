2a1ea1ebfd399818e2a49c0f1ff2a46898ada866
Jookmaste <punnlaowatchara@gmail.com>
2026-08-10 15:42:22 +0700

    Push

--- comBaseAssignment5/src/main.ts
+++ comBaseAssignment5/src/main.ts
@@ -9,9 +9,7 @@ import 'nprogress/nprogress.css'
 const app = createApp(App)
 
 import { inject } from '@vercel/analytics'
-import { injectSpeedInsights } from '@vercel/speed-insights'
 inject()
-injectSpeedInsights()
 
 app.use(createPinia())
 app.use(router)
